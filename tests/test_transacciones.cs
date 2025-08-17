d3c83d46d19b11db725c7c2f56dd092024fe428b
Zelidee <zelideeguemez@gmail.com>
2025-08-17 13:46:33 -0400

    feature: creacion de pruebas de transacciones y concurrencia

--- tests/test_transacciones.cs
+++ tests/test_transacciones.cs
@@ -1,17 +1,29 @@
-﻿/*using Xunit;
+﻿using SimulacionSucursalesBanco.src;
+using Xunit;
 
-public class TestTransacciones
+public class CuentaTests
 {
     [Fact]
-    public void Deposito_AumentaSaldo()
+    public void Depositar_SumaCorrectamente()
     {
-        // Pendiente de implementación cuando la clase Cuenta esté lista
+        var cuenta = new Cuenta(1, "Test", TipoCuenta.Ahorro, 1000m);
+        Assert.True(cuenta.Depositar(500m));
+        Assert.Equal(1500m, cuenta.ConsultarSaldo());
     }
 
     [Fact]
-    public void Retiro_DisminuyeSaldo()
+    public void Retirar_DescuentaCorrectamente()
     {
-        // Pendiente de implementación
+        var cuenta = new Cuenta(2, "Test", TipoCuenta.Ahorro, 1000m);
+        Assert.True(cuenta.Retirar(400m));
+        Assert.Equal(600m, cuenta.ConsultarSaldo());
+    }
+
+    [Fact]
+    public void Retirar_SaldoInsuficiente_Falla()
+    {
+        var cuenta = new Cuenta(3, "Test", TipoCuenta.Ahorro, 100m);
+        Assert.False(cuenta.Retirar(200m));
+        Assert.Equal(100m, cuenta.ConsultarSaldo());
     }
 }
-*/
